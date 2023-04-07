53abe7bb00b8f81d6ca4ce4b6dc21537a605faad
Roberto Walter <roberto.wtr@gmail.com>
2023-04-07 16:43:50 -0300

    Setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+#pip install git+https://github.com/robertowtr/rwtr-utils.git
+setup(
+    name='rwtr-utils',
+    packages=['rwtr-utils'],
+    version='0.1',
+    author='Roberto Walter',
+    author_email='roberto.wtr@gmail.com',
+    description='My personal toolbox.',
+    long_description_content_type="text/markdown",
+    url='https://github.com/robertowtr/rwtr-utils',
+    project_urls = {
+        "Bug Tracker": "https://github.com/robertowtr/rwtr-utils/issues"
+    }
+)
